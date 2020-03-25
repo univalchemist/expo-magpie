c69af0764a5b5d734e48c9240ff4086dc1fd8780
lisheng46 <lisheng19940406@gmail.com>
2020-03-26 01:37:55 +0800

    test

--- ReduxApp.js
+++ ReduxApp.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AppContainer from './src/router/index'
 
 const ReduxApp = ({ }) => {
+    console.log('Hello')
     return [
         <AppContainer key={1} uriPrefix="/app" />,
     ]
