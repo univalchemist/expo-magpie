3a5ef8a26d6ae4b20d0b1b5354a5fc36b43f9f6c
sun20173600 <xiaoxing440824@outlook.com>
2020-03-26 01:39:51 +0800

    Test

--- ReduxApp.js
+++ ReduxApp.js
@@ -2,7 +2,7 @@ import React from 'react';
 import AppContainer from './src/router/index'
 
 const ReduxApp = ({ }) => {
-    console.log('Hello')
+    console.log('Hello World')
     return [
         <AppContainer key={1} uriPrefix="/app" />,
     ]
